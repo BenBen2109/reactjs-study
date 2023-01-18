c7217837ee8dd53bc03a123b2978d98ef4647d41
BenBen2109 <72802198+BenBen2109@users.noreply.github.com>
2023-01-18 09:44:29 +0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './components/Button';
-import List from './components/List';
 
 function App() {
-  const cars = ['BMW', 'Honda', 'Merc']
-  const listRender = (item) => <li key={item}>{item}</li>
+  //can use callback at initial state
+  const [counter, setCounter] = useState(() => {
+    const defaultCounter = 10
+    return defaultCounter
+  })
+  const handleIncrease = () => {
+    //Use callback to call more than 1 time
+    //setCounter(prev => prev + 1)
+    setCounter(counter + 1)
+  }
+  const handleDecrease = () => {
+    setCounter(counter - 1)
+  }
   return (
-
     <React.Fragment>
-      {React.createElement('h1', null, 'Haha')}
+      {React.createElement('h1', null, 'Counter')}
+      <div>{counter}</div>
       <Button
-        title='Click Me!' href='https://facebook.com'
-        onClick={
-          () => console.log(Math.random())}>
-        asdasd
+        title='Increase'
+        onClick={handleIncrease}>
       </Button>
-      <List data={cars}>{listRender}</List>
+      <Button
+        title='Decrease'
+        onClick={handleDecrease}>
+      </Button>
     </React.Fragment>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 
 export default function Button({ title, href, onClick, children }) {
-    const Component = href ? 'a' : 'button'
-    const props = {}
-    if(href) {
-        props.href = href
-    }
-    if(onClick) {
-        props.onClick = onClick
-    }
+  const Component = href ? 'a' : 'button'
+  const props = {}
+  if (onClick) {
+    props.onClick = onClick
+  }
   return (
-    <Component {...props}>{children}</Component>
+    <Component {...props}>{title}</Component>
   )
 }
